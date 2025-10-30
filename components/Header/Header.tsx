df2b24735ca0c41bea1b4b56cae76378e1fd3ed5
HaJaehyeong <hajae305@gmail.com>
2025-10-30 10:46:43 +0900

    HAJAE-14 modify tailwind: theme and config

---
+++ components/Header/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="h-16 w-full sticky top-0 shadow-md z-50 flex items-center px-4">
+      <div className="text-xl font-bold">My Header</div>
+      <div className="bg-primary">보임???</div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,60 +5,6 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   darkMode: ['class', '[data-theme="dark"]'],
   content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
-  theme: {
-    theme: {
-      extend: {
-        colors: {
-          primary: 'var(--primary)',
-          secondary: 'var(--secondary)',
-          text: 'var(--text)',
-          info: 'var(--info)',
-          success: 'var(--success)',
-          error: 'var(--error)',
-          warning: 'var(--warning)',
-          border: 'var(--border)',
-          background: 'var(--background)',
-
-          'primary-hover': 'var(--primary-hover)',
-          'secondary-hover': 'var(--secondary-hover)',
-          'primary-border': 'var(--primary-border)',
-          'secondary-border': 'var(--secondary-border)',
-
-          'text-secondary': 'var(--text-secondary)',
-          'text-muted': 'var(--text-muted)',
-
-          'background-alt': 'var(--background-alt)',
-          'background-hover': 'var(--background-hover)',
-          'background-disabled': 'var(--background-disabled)',
-
-          'btn-primary-text': 'var(--btn-primary-text)',
-          'btn-secondary-text': 'var(--btn-secondary-text)',
-          'btn-disabled-bg': 'var(--btn-disabled-bg)',
-          'btn-disabled-text': 'var(--btn-disabled-text)',
-
-          surface: 'var(--surface)',
-          'surface-border': 'var(--surface-border)',
-          'surface-hover': 'var(--surface-hover)',
-
-          'input-bg': 'var(--input-bg)',
-          'input-border': 'var(--input-border)',
-          'input-focus': 'var(--input-focus)',
-
-          overlay: 'var(--overlay)',
-          'gradient-primary': 'var(--gradient-primary)',
-
-          'card-bg': 'var(--card-bg)',
-          'card-border': 'var(--card-border)',
-        },
-
-        boxShadow: {
-          sm: 'var(--shadow-sm)',
-          md: 'var(--shadow-md)',
-          lg: 'var(--shadow-lg)',
-        },
-      },
-    },
-  },
   plugins: [forms, typography],
 };
 export default config;
