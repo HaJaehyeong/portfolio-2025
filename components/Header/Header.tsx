18cb1373f0065ae9175ffbf952d42ea0a964b855
HaJaehyeong <hajae305@gmail.com>
2025-11-03 08:48:40 +0900

    HAJAE-14 modify style: add justify-between in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <header className="h-16 w-full sticky top-0 shadow-md flex flex-col justify-center items-center px-4 bg-background/80 border-b border-border backdrop-blur-sm backdrop-saturate-200 z-1000 dark:bg-background/50">
-        <nav className="relative max-w-[1400px] w-full flex flex-1 items-center">
+        <nav className="relative max-w-[1400px] w-full flex flex-1 items-center justify-between">
           <div className="text-xl font-bold">My Header</div>
           <ThemeToggle />
         </nav>
